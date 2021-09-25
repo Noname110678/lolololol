dba726751080d4cb1d684561d8bdcb4cb6bbb3d1
Noname110678 <79567458+Noname110678@users.noreply.github.com>
2021-09-25 20:35:11 +0500

    i made header, profile, navbar and react app it works unlike the old projects ez

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Header(){
+    return (
+        <div>
+            <h1>You are looking at Header.js [Header(){}]</h1>
+        </div>
+    )
+}
+export default Header;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Navbar(){
+    return (
+        <div>
+            <h1>Welcome to navbar</h1>
+        </div>
+    )
+}
+export default Navbar;
